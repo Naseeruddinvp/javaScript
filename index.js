9d6a0f95164956cf5134337824934a64e90fc043
Naseeruddinvp <134862356+Naseeruddinvp@users.noreply.github.com>
2023-05-28 23:23:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     $("#name").text(profile.getName());
     $("#email").text(profile.getEmail());
-    $("#image").attribute('src', profile.getImageurl());
+    $("#image").attr('src', profile.getImageUrl());
     $(".data").css("display","block");
     $(".g-signin2").css("display", "none");
 
@@ -11,7 +11,7 @@ function onSignIn(googleUser) {
   function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-      alert("You have been SignedOut successfully");
+      alert("You have been Signed Out successfully");
       $(".g-signin2").css("display", "block");
       $(".data").css("display","none");
 
